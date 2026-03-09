6958c5813c06da7ae55dfcb5ca807ed30e9d53d1
Aritra <aritra.03@proton.me>
2026-03-09 11:40:39 +0530

    Added get-key route

--- routes/get-key.js
+++ routes/get-key.js
@@ -7,9 +7,25 @@ const getkey = new Hono();
 getkey.get("/get-key", async function(c){
     try{
 
+        const { keyId } = await c.req.json();
+        if(!keyId)
+            return c.json({message: "Key Id not provided"}, 400);
 
+        const sql = neon(c.env.DATABASE_URL);
+        const data = await sql.query(`
+            SELECT * FROM api_keys WHERE key_id = $1`,
+            [keyId]
+        );
 
-        return c.json({message: "Get success"}, 200);
+        if(data.length === 0)
+            return c.json({message: "No Key Found"}, 404);
+
+        const decryptedKey = await decryptKey(c, data[0].encrypted_key);
+        // const decryptedKey = await decryptKey(data[0].);
+        return c.json({
+            message: "Decryption Success",
+            key: decryptedKey
+        }, 200);
     }
     catch(error){
         console.log(error);
